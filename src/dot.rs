6498cf452170af4691089d10bb3da0f625812c8e
samsja <sami.jaghouar@hotmail.fr>
2023-03-25 14:25:26 +0100

    feat: use view in dot_3d
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- src/dot.rs
+++ src/dot.rs
@@ -1,6 +1,6 @@
-use ndarray::{Array, Ix2, Ix3, NdFloat};
+use ndarray::{Array, ArrayView, Ix2, Ix3, NdFloat};
 
-pub fn dot_3d<'a, T: NdFloat>(mat1: &'a Array<T, Ix3>, mat2: &'a Array<T, Ix2>) -> Array<T, Ix3> {
+pub fn dot_3d<'a, T: NdFloat>(mat1: &'a Array<T, Ix3>, mat2: &'a ArrayView<T, Ix2>) -> Array<T, Ix3> {
     let shape = mat1.shape();
 
     let mat1_2d = mat1.to_shape((shape[0] * shape[1], shape[2])).unwrap();
@@ -24,7 +24,7 @@ mod tests {
     fn test_dot_3d() {
         let mat1 = Array::<f32, _>::zeros((3, 10, 12).f());
         let mat2 = Array::<f32, _>::zeros((12, 15).f());
-        let mat3 = dot_3d(&mat1, &mat2);
+        let mat3 = dot_3d(&mat1, &mat2.view());
 
         assert_eq!(mat3.shape(), &[3, 10, 15]);
     }
