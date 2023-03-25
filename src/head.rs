d485d33a0aefb8ff1f9c0894aec59511f9a4bf9d
samsja <sami.jaghouar@hotmail.fr>
2023-03-25 11:09:39 +0100

    refactor: lowercase for var name
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- src/head.rs
+++ src/head.rs
@@ -5,9 +5,9 @@ pub struct Head<T>
 where
     T: NdFloat,
 {
-    w_Q: Array<T, Ix2>,
-    w_K: Array<T, Ix2>,
-    w_V: Array<T, Ix2>,
+    w_q: Array<T, Ix2>,
+    w_k: Array<T, Ix2>,
+    w_v: Array<T, Ix2>,
     dim_key: usize,
     dim_val: usize,
 }
@@ -17,29 +17,29 @@ where
     T: NdFloat,
 {
     pub fn new_zeros(embed_dim: usize, dim_key: usize, dim_val: usize) -> Head<T> {
-        let w_Q = Array::<T, _>::zeros((embed_dim, dim_key));
-        let w_K = Array::<T, _>::zeros((embed_dim, dim_key));
-        let w_V = Array::<T, _>::zeros((embed_dim, dim_val));
+        let w_q = Array::<T, _>::zeros((embed_dim, dim_key));
+        let w_k = Array::<T, _>::zeros((embed_dim, dim_key));
+        let w_v = Array::<T, _>::zeros((embed_dim, dim_val));
 
         Head {
-            w_Q,
-            w_K,
-            w_V,
+            w_q,
+            w_k,
+            w_v,
             dim_key,
             dim_val,
         }
     }
 
     pub fn attention(&self, input: &Array<T, Ix2>) -> Array<T, Ix2> {
-        let Q = input.dot(&self.w_Q);
-        let K = input.dot(&self.w_K);
-        let V = input.dot(&self.w_V);
+        let q = input.dot(&self.w_q);
+        let k = input.dot(&self.w_k);
+        let v = input.dot(&self.w_v);
 
-        let scores = Q.dot(&K.t()) / T::from(self.dim_key).unwrap().sqrt();
+        let scores = q.dot(&k.t()) / T::from(self.dim_key).unwrap().sqrt();
         let exp_scores = scores.mapv(|a| a.exp());
 
         let output = exp_scores.clone() / exp_scores.sum();
 
-        output.dot(&V)
+        output.dot(&v)
     }
 }
