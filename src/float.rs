86d2426c9d742757e42101e1627ef8db5b01c3a1
samsja <sami.jaghouar@hotmail.fr>
2023-03-28 21:21:46 +0200

    feat: use my fork of ndarray
    Signed-off-by: samsja <sami.jaghouar@hotmail.fr>

--- src/float.rs
+++ src/float.rs
@@ -2,14 +2,6 @@ use half::{bf16, f16};
 use ndarray::ScalarOperand;
 use num_traits::Float;
 
-struct Myf16(f16);
-struct Mybf16(bf16);
-
-impl ScalarOperand for Myf16 {}
-impl ScalarOperand for Mybf16 {}
-
-
-
 pub trait MyFloat: 'static + Float + ScalarOperand {}
 
 impl MyFloat for f32 {}
